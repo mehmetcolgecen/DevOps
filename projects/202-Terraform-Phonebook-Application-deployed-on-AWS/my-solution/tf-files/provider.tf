1fc6dbdc9768de189829db73fe8fd8fe6b2ca8f4
C7136-Mehmet <mehmetcolgecen5080@gmail.com>
2021-06-06 08:54:28 +0300

    project 202 solution

---
+++ projects/202-Terraform-Phonebook-Application-deployed-on-AWS/my-solution/tf-files/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "3.44.0"
+    }
+    github = {
+      source = "integrations/github"
+      version = "4.10.1"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+provider "github" {
+  token = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+}
