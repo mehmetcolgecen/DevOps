e2e059c10191fcf299ebf938d47d82143fbae145
C7136-Mehmet <mehmetcolgecen5080@gmail.com>
2021-06-06 08:54:28 +0300

    project 202 solution

---
+++ projects/202-Terraform-Phonebook-Application-deployed-on-AWS/my-solution/Dont-Forget.md
@@ -0,0 +1,5 @@
+Dont forger these things;
+1. Create new credentials and add aws credentials with aws config
+2. Add github token to provider.tf
+3. Add github token to user-data.sh
+4. Creating a empty github repo and push all the files inside phonebook folder 
--- projects/202-Terraform-Phonebook-Application-deployed-on-AWS/my-solution/tf-files/user-data.sh
+++ projects/202-Terraform-Phonebook-Application-deployed-on-AWS/my-solution/tf-files/user-data.sh
@@ -4,6 +4,6 @@ yum install python3 -y
 pip3 install flask
 pip3 install flask_mysql
 yum install git -y
-TOKEN="xxxxxxxxxxxxx"
-cd /home/ec2-user && git clone https://$TOKEN@github.com/timothy-clark/phonebook.git
+TOKEN="xxxxxxxxxxxxxxxxxxxxxxxxxxx"
+cd /home/ec2-user && git clone https://$TOKEN@github.com/mehmetcolgecen/phonebook.git
 python3 /home/ec2-user/phonebook/phonebook-app.py
