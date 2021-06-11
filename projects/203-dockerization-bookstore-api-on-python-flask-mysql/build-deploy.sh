d30b57585f70917e85ee1359abc76dfda2350b30
C7136-Mehmet <mehmetcolgecen5080@gmail.com>
2021-06-11 14:59:44 +0300

    project 203

--- projects/203-dockerization-bookstore-api-on-python-flask-mysql/build-deploy.sh
+++ projects/203-dockerization-bookstore-api-on-python-flask-mysql/build-deploy.sh
@@ -8,12 +8,11 @@ curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compo
 -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 mkdir -p /home/ec2-user/bookstore-api
-TOKEN="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"
-FOLDER="https://$TOKEN@raw.githubusercontent.com/mehmetcolgecen/DevOps/main/projects/203-dockerization-bookstore-api-on-python-flask-mysql/"
+TOKEN="" #"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@"
+FOLDER="https://${TOKEN}raw.githubusercontent.com/mehmetcolgecen/DevOps/main/projects/203-dockerization-bookstore-api-on-python-flask-mysql/"
 curl -s --create-dirs -o "/home/ec2-user/bookstore-api/app.py" -L "$FOLDER"bookstore-api.py
 curl -s --create-dirs -o "/home/ec2-user/bookstore-api/requirements.txt" -L "$FOLDER"requirements.txt
 curl -s --create-dirs -o "/home/ec2-user/bookstore-api/Dockerfile" -L "$FOLDER"Dockerfile
 curl -s --create-dirs -o "/home/ec2-user/bookstore-api/docker-compose.yml" -L "$FOLDER"docker-compose.yml
 cd /home/ec2-user/bookstore-api
-docker build -t callahanclarus/bookstore-api:latest .
-docker-compose up -d
+docker-compose up -d --build
--- projects/203-dockerization-bookstore-api-on-python-flask-mysql/terraform-deploy.tf
+++ projects/203-dockerization-bookstore-api-on-python-flask-mysql/terraform-deploy.tf
@@ -19,26 +19,7 @@ resource "aws_instance" "tf-docker-ec2" {
   tags = {
     Name = "Bookstore-Web-Server"
   }
-  user_data = <<-EOF
-          #! /bin/bash
-          yum update -y
-          amazon-linux-extras install docker -y
-          systemctl start docker
-          systemctl enable docker
-          usermod -a -G docker ec2-user
-          curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" \
-          -o /usr/local/bin/docker-compose
-          chmod +x /usr/local/bin/docker-compose
-          mkdir -p /home/ec2-user/bookstore-api
-          TOKEN="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"
-          FOLDER="https://$TOKEN@raw.githubusercontent.com/mehmetcolgecen/DevOps/main/projects/203-dockerization-bookstore-api-on-python-flask-mysql/"
-          curl -s --create-dirs -o "/home/ec2-user/bookstore-api/app.py" -L "$FOLDER"bookstore-api.py
-          curl -s --create-dirs -o "/home/ec2-user/bookstore-api/requirements.txt" -L "$FOLDER"requirements.txt
-          curl -s --create-dirs -o "/home/ec2-user/bookstore-api/Dockerfile" -L "$FOLDER"Dockerfile
-          curl -s --create-dirs -o "/home/ec2-user/bookstore-api/docker-compose.yml" -L "$FOLDER"docker-compose.yml
-          cd /home/ec2-user/bookstore-api
-          docker-compose up -d --build
-          EOF
+  user_data = file("build-deploy.sh")
 }
 
 resource "aws_security_group" "tf-docker-sec-gr-202" {
