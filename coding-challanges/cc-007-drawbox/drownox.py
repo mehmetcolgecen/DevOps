11cd026a29e01fd12236b1169691b67e957996b0
C7136-Mehmet <mehmetcolgecen5080@gmail.com>
2021-07-03 17:01:09 +0300

    update

--- coding-challanges/cc-007-drawbox/drownox.py
+++ coding-challanges/cc-007-drawbox/drownox.py
@@ -3,5 +3,10 @@ for i in range(x):
     if i == 0 or i == x-1:
         print("#" * x)
     else:
-        print("{0}{1}{0}".format('#', (x-2)*" "))
-        #print("#" + " "*(x-2) + "#")
+        print("#" + " "*(x-2) + "#")
+        
+        
+        
+        
+        
+#        print("{0}{1}{0}".format('#', (x-2)*" "))
